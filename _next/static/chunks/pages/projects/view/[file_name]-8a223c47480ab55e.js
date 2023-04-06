d66e1dfae9d73c2c27484ac8d84829514991dd49
biplobsd <biplobsd@users.noreply.github.com>
2023-04-06 20:50:29 +0000

    deploy: c67115a4bc68ffb51309124f28edff8c70298fdf

---
+++ _next/static/chunks/pages/projects/view/[file_name]-8a223c47480ab55e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[809],{9426:function(n,_,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/projects/view/[file_name]",function(){return e(2316)}])}},function(n){n.O(0,[929,664,675,316,774,888,179],function(){return n(n.s=9426)}),_N_E=n.O()}]);
