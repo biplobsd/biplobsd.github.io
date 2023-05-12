b44d160bcf26e0a625b5e54632431f4ba279eda1
biplobsd <biplobsd@users.noreply.github.com>
2023-05-12 13:35:45 +0000

    deploy: 7211df2ccd76e20cbabbba8043a3958ddce4d81b

--- _next/static/chunks/1944.f7a8c68801a8f27c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1944],{1944:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var r=t(5893),a=t(5152),u=t.n(a);let l=u()(()=>Promise.all([t.e(4416),t.e(1846)]).then(t.bind(t,1846)),{loadableGenerated:{webpack:()=>[1846]}});function c(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)(l,{})})}}}]);
